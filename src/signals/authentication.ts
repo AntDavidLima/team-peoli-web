70992a8eac07d47930ada1e2218eb6dab320c525
David Lima <antdavidlima@gmail.com>
2024-04-20 13:10:16 -0300

    fix(authentication): fix user redirection on login and logout

--- src/signals/authentication.ts
+++ src/signals/authentication.ts
@@ -3,16 +3,16 @@ import { computed, effect, signal } from "@preact/signals";
 import { Cookies } from "react-cookie";
 
 interface LoginForm {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 interface APIAuthenticateResponse {
-	auth_token: string;
+  auth_token: string;
 }
 
 interface CurrentUser {
-	authToken: string;
+  authToken: string;
 }
 
 const cookies = new Cookies();
@@ -22,21 +22,24 @@ export const currentUser = signal<CurrentUser | null>(null);
 export const isAuthenticated = computed(() => currentUser.value !== null);
 
 effect(() => {
-	if (!currentUser.value) {
-		const authToken = cookies.get("auth_token");
+  if (!currentUser.value) {
+    const authToken = cookies.get("auth_token");
 
-		currentUser.value = { authToken } || null;
-	} else {
-		api.defaults.headers.common["Authorization"] =
-			`Bearer ${currentUser.value.authToken}`;
-	}
+    if (authToken) {
+      currentUser.value = { authToken };
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${authToken}`;
+    }
+  }
 });
 
 export async function login({ email, password }: LoginForm) {
-	const { data } = await api.post<APIAuthenticateResponse>(`/authentication`, {
-		email,
-		password,
-	});
+  const { data } = await api.post<APIAuthenticateResponse>(`/authentication`, {
+    email,
+    password,
+  });
 
-	cookies.set("auth_token", data.auth_token, { sameSite: "strict" });
+  cookies.set("auth_token", data.auth_token, { sameSite: "strict" });
+
+  currentUser.value = { authToken: data.auth_token };
 }
