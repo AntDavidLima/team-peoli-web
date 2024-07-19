01051a279aa655813aaf1ce360033d2c65bfa6b2
David Lima <antdavidlima@gmail.com>
2024-07-18 23:18:27 -0300

    fix(dev): remove dev tools from screen

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,11 @@
 import { Toaster } from "@/components/ui/toaster";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
 			<Outlet />
 			<Toaster />
-			<TanStackRouterDevtools />
-			<ReactQueryDevtools />
 		</>
 	),
 });
